910bc28a3015612ad60c547d1e01a10bf57ee67f
Alissa <Alissalikavec@gmail.com>
2014-07-07 23:07:13 -0400

    Indentation and closing fix

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,11 +1,12 @@
 class ApiController < ApplicationController
-	private
+private
 	def authenticated?
     	authenticate_with_http_basic {|user, pass| User.where( username: user, password: pass).present? }
 	end
 
   	def check_auth
-  	unless authenticated?
-    	render json: {}, status: :forbidden #403  
+  		unless authenticated?
+    		render json: {}, status: :forbidden #403  
+    	end
   	end
 end
